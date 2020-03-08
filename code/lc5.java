f93a635a396b1fb03ab554e0e15a6034f0cd09f0
Mryang11 <yyxyyxyang@126.com>
2020-03-08 10:27:55 +0800

    update lc5

--- code/lc5.java
+++ code/lc5.java
@@ -22,13 +22,62 @@ public class lc5 {
         for (int i = n - 1; i >= 0; i--) {
             for (int j = i; j < n; j++) {
                 if (s.charAt(i) == s.charAt(j)) {
-                    if ((j - i) < 3 || dp[i + 1][j - 1])
+                    if ((j - i) < 3 || dp[i + 1][j - 1]) {
                         dp[i][j] = true;
-                    if (dp[i][j] && j - i + 1 > res.length())
-                        res = s.substring(i, j + 1);   // 起始索引，终止索引(不包括，所以+1)
+                    }
+                    if (dp[i][j] && j - i + 1 > res.length()) {
+                        // 起始索引，终止索引(不包括，所以+1)
+                        res = s.substring(i, j + 1);
+                    }
                 }
             }
         }
         return res;
+    }
+
+    /**
+     * 事实上，只需使用恒定的空间，我们就可以在
+     * O(n*n) 的时间内解决这个问题。
+     *
+     * 我们观察到回文中心的两侧互为镜像。因此，回文可以从它的中心展开，并且只有
+     * 2n−1 个这样的中心。
+     *
+     * 你可能会问，为什么会是2n−1 个，而不是n个中心？原因在于所含字母数为偶数的回文的中心可以处于两字母之间（例如
+     * “abba”
+     * “abba” 的中心在两个
+     * ‘b’
+     * ‘b’ 之间）。
+     *
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/longest-palindromic-substring/solution/zui-chang-hui-wen-zi-chuan-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * @param s
+     * @return
+     */
+    public String longestPalindrome1(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
     }
 }
