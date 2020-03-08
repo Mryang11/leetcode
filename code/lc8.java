a87ba07e45b7c40dbffcd28e5611d393f0f7c7a3
Mryang11 <yyxyyxyang@126.com>
2020-03-08 11:05:18 +0800

    update lc8

--- code/lc8.java
+++ code/lc8.java
@@ -1,11 +1,14 @@
 package code;
 
-/*
+/**
  * 8. String to Integer (atoi)
  * 题意：字符串转数字，按题目要求转换。(题出的不好，没什么意义)
  * 难度：Medium
  * 分类：Math, String
  * 注意：如何判断溢出
+ *
+ * 正则
+ * ^\s*([+-]?\d+)
  */
 public class lc8 {
     public static void main(String[] args) {
@@ -13,29 +16,50 @@ public class lc8 {
     }
 
     public static int myAtoi(String str) {
-        char[] s = str.toCharArray();
+        int rev = 0;
+        char[] charList = str.toCharArray();
+        int length = str.length();
+        int zf = 1;
         int i = 0;
-        int num = 0;
-        int sign = 1;
-        while (i < s.length && s[i] == ' ')
-            i++;
-        if (i < s.length && s[i] == '+') {
-            sign = 1;
-            i++;
-        } else if (i < s.length && s[i] == '-') {
-            sign = -1;
-            i++;
+        int pop = 0;
+        for (; i < length; i++) {
+            if (charList[i] == ' ') {
+                continue;
+            } else {
+                if (charList[i] == '-') {
+                    i++;
+                    zf = -1;
+                    break;
+                }
+                if (charList[i] == '+') {
+                    i++;
+                    break;
+                }
+                if (charList[i] < '0' || charList[i] > '9') {
+                    return 0;
+                } else {
+                    break;
+                }
+            }
         }
-        while (i < s.length && s[i] >= '0' && s[i] <= '9') {
-            //sign 乘了两次，保证num为正，以便*10+下一位
-            int temp = sign * (num * 10 + Integer.parseInt(String.valueOf(s[i])));
-            if (sign == 1 && temp / 10 * sign != num)
+        if (i == length) {
+            return 0;
+        }
+        for (; i < length; i++) {
+            if (charList[i] < '0' || charList[i] > '9') {
+                return rev;
+            }
+            pop = (charList[i] - 48) * zf;
+            // 2的31次方-1的个位数 = 7
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) {
                 return Integer.MAX_VALUE;
-            else if (sign == -1 && temp / 10 * sign != num)
+            }
+            // -2的31次方的个位数 = 8
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) {
                 return Integer.MIN_VALUE;
-            num = temp * sign;
-            i++;
+            }
+            rev = rev * 10 + pop;
         }
-        return sign * num;
+        return rev;
     }
 }
