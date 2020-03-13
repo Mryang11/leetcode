d0d2fd4885423b2873271bb8d903cbc07d7f867e
Mryang11 <yyxyyxyang@126.com>
2020-03-13 16:49:39 +0800

    update lc17

--- code/lc17.java
+++ code/lc17.java
@@ -1,13 +1,14 @@
 package code;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
-/*
+/**
  * 17. Letter Combinations of a Phone Number
  * 题意：手机键盘字母输入
  * 难度：Medium
- * 思路：思路记一下，每次放到vector中，遍历vector,加上所有的可能
+ * 思路：深度优先DFS
  * 分类：String, Backtracking
  */
 public class lc17 {
@@ -16,22 +17,46 @@ public class lc17 {
     }
 
     public static List<String> letterCombinations(String digits) {
-        Vector<String> res = new Vector<>();
-        if (digits.length() == 0)
-            return res;
-        String[] charmap = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        res.add("");
-        for (int i = 0; i < digits.length(); i++) {
-            int n = digits.charAt(i) - '0';
-            Vector<String> temp_res = new Vector<>();
-            for (int j = 0; j < charmap[n].length(); j++) {
-                for (int k = 0; k < res.size(); k++) {
-                    temp_res.add(res.get(k) + charmap[n].charAt(j));
-                }
-            }
-
-            res = temp_res;
+        List<String> list = new ArrayList<>();
+        if (digits == null || "".equals(digits)) {
+            return list;
         }
-        return res;
+        String nowStr = "";
+        addCharacter(digits, 0, nowStr, list);
+        return list;
+    }
+
+    private static void addCharacter(String digits, int key, String nowStr, List<String> list) {
+        if (digits.length() == key) {
+            list.add(nowStr);
+            return;
+        }
+        char[] chars = getChars(digits.charAt(key));
+        if (chars != null) {
+            for (int i = 0; i < chars.length; i++) {
+                addCharacter(digits, key + 1, nowStr + chars[i], list);
+            }
+        }
+    }
+
+    private static char[] getChars(char charAt) {
+        if ('2' == charAt) {
+            return new char[]{'a', 'b', 'c'};
+        } else if ('3' == charAt) {
+            return new char[]{'d', 'e', 'f'};
+        } else if ('4' == charAt) {
+            return new char[]{'g', 'h', 'i'};
+        } else if ('5' == charAt) {
+            return new char[]{'j', 'k', 'l'};
+        } else if ('6' == charAt) {
+            return new char[]{'m', 'n', 'o'};
+        } else if ('7' == charAt) {
+            return new char[]{'p', 'q', 'r', 's'};
+        } else if ('8' == charAt) {
+            return new char[]{'t', 'u', 'v'};
+        } else if ('9' == charAt) {
+            return new char[]{'w', 'x', 'y', 'z'};
+        }
+        return null;
     }
 }
